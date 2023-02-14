d51ebf3c5aaf8da5733aee35448c2484d65de74c
Pwongchaiya <68671925+Pwongchaiya@users.noreply.github.com>
2023-02-14 11:13:33 -0800

    deleted notes

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,7 +3,6 @@ import React from "react"
 import Header from "./Header"
 import Note from "./Note"
 import Footer from "./Footer"
-import notes from "../notes"
 import CreateArea from "./CreateArea"
 
 function App() {
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import StickyNote2Icon from '@mui/icons-material/StickyNote2';
 function Header(props) { 
     return <header>
-        <h1><StickyNote2Icon/> Keeper </h1>
+        <h1><StickyNote2Icon/> Note Pad </h1>
     </header>
  }
 
